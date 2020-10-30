c851d3c17303ab954a7af772f3b8bdd5a4d2c277
andrewKarachun0304 <andrusha00765@gmail.com>
2020-10-30 14:42:02 +0300

    work location update1

--- app/src/main/java/com/example/weathertz/MainActivity.kt
+++ app/src/main/java/com/example/weathertz/MainActivity.kt
@@ -17,7 +17,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 private const val PERMISSION_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
 private const val PERMISSION_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
-private const val PERMISSION_ID = 1010
+private const val PERMISSIONS_REQUEST_ACCESS_LOCATION = 1010
 
 class MainActivity : AppCompatActivity(), WeatherListFragment.Listener {
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -29,32 +29,26 @@ class MainActivity : AppCompatActivity(), WeatherListFragment.Listener {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        requestPermission()
-        getLastLocation()
+        checkPermission()
     }
 
-    private fun checkPermission(): Boolean {
+    private fun checkPermission() {
         if (ContextCompat.checkSelfPermission(this, PERMISSION_FINE_LOCATION) ==
             PackageManager.PERMISSION_GRANTED ||
             ContextCompat.checkSelfPermission(this, PERMISSION_COARSE_LOCATION) ==
             PackageManager.PERMISSION_GRANTED
         ) {
-            return true
+            getLastLocation()
         } else {
-
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                PERMISSIONS_REQUEST_ACCESS_LOCATION
+            )
         }
-        return false
-    }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ),
-            PERMISSION_ID
-        )
     }
 
     private fun isLocationEnabled(): Boolean {
@@ -65,23 +59,19 @@ class MainActivity : AppCompatActivity(), WeatherListFragment.Listener {
 
     @SuppressLint("MissingPermission")
     private fun getLastLocation() {
-        if (checkPermission()) {
-            if (isLocationEnabled()) {
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
-                    val location: Location? = task.result
-                    if (location == null) {
-                        newLocationData()
-                    } else {
-                        latitude_text.text =
-                            "You Current Location is : Long: " + location.longitude + " , Lat: " + location.latitude
-                    }
+        if (isLocationEnabled()) {
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                val location: Location? = task.result
+                if (location == null) {
+                    newLocationData()
+                } else {
+                    latitude_text.text =
+                        "You Current Location is : Long: " + location.longitude + " , Lat: " + location.latitude
                 }
-            } else {
-                Toast.makeText(this, "Please Turn on Your device Location", Toast.LENGTH_SHORT)
-                    .show()
             }
         } else {
-            requestPermission()
+            Toast.makeText(this, "Please Turn on Your device Location", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
@@ -104,6 +94,20 @@ class MainActivity : AppCompatActivity(), WeatherListFragment.Listener {
             val lastLocation: Location = locationResult.lastLocation
             latitude_text.text =
                 "You Last Location is : Long: " + lastLocation.longitude + " , Lat: " + lastLocation.latitude
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLastLocation()
+                }
+            }
         }
     }
 
